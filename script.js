86165ed5cf191a34400ac355b9c0230990f6ddfb
Neo3691102 <nestor8772@gmail.com>
2025-03-27 16:43:19 -0600

    Se agrega evento al pulsar enter en el input y se crea código para separar por digitos el numero

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
+const numconvertido = [];
+
+const romanos = {
+  1: "I",
+  2: "II",
+  3: "III",
+  4: "IV",
+  5: "V",
+  6: "VI",
+  7: "VII",
+  8: "VIII",
+  9: "IX",
+  10: "X",
+  40: "XL",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M",
+};
+
 const cleanInput = (input) => {
   if (!input) {
     output.textContent = "Por favor introduce un numero válido.";
@@ -26,14 +49,36 @@ const cleanInput = (input) => {
 convertBtn.addEventListener("click", () => {
   const numLimpio = cleanInput(numberInput.value);
   console.log(numLimpio);
-  
-  if (numLimpio > 10 && numLimpio <= 100) {
-    console.log("mayor que 10");
-  } else if (numLimpio > 100) {
-    console.log("mayor que 100");
-  } else if (numLimpio > 1000) {
-    console.log("mayor que 1000");
-  } else {
-    console.log("menor o igual a 10");
+
+  const numLimToStr = numLimpio.toString();
+  console.log(numLimToStr);
+});
+
+numberInput.addEventListener("keydown", (e) => {
+  // console.log(romanos[1]);
+  if (e.key === "Enter") {
+    const numLimpio = cleanInput(numberInput.value);
+    console.log(numLimpio);
+
+    const numLimToStr = numLimpio.toString(); //convierte numLimpio en cadena para poder manipular
+    console.log(numLimToStr);
+    const arrNumLim = numLimToStr.split(""); //convertimos numLimToStr a un array
+
+    if (arrNumLim.length > 1) {
+      //si el numero es de dos digitos
+
+      console.log("numero de dos digitos");
+
+      for (const romano in romanos) {
+        if (romano === arrNumLim[1]) {
+          console.log(romanos[romano]); //vemos cual romano coincidio
+          numconvertido.push(romanos[romano]);
+          console.log(numconvertido); //numero convertido
+        }
+      }
+    }
+    console.log(arrNumLim);
+
+    numberInput.value = "";
   }
 });
